256e9f855b550811a3dd3fb714119cc38d728b76
amitatiwari255 <amita.tiwari255@gmail.com>
2023-01-28 14:12:57 +0530

    Added :Program  To create Single Linked List

---
+++ src/main/LinkedList.java
@@ -0,0 +1,23 @@
+public class LinkedList {
+    Node head;
+    static class Node {
+        int value;
+        Node next;
+        Node(int data) {
+            value = data;
+        }
+    }
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList();
+        linkedList.head = new Node(56);
+        Node second = new Node(30);
+        Node third = new Node(70);
+        linkedList.head.next = second;
+        second.next = third;
+        System.out.print("LinkedList: ");
+        while (linkedList.head != null) {
+            System.out.print(linkedList.head.value + " ");
+            linkedList.head = linkedList.head.next;
+        }
+    }
+}
